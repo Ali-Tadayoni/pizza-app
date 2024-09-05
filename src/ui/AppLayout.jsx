ad5579e1db23ca97905eb876f61dc22aab4c57e1
“Ali <tadayoni.ali.enayat@gmail.com>
2024-09-05 10:46:27 +0330

    define children routes for appLayout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <h1>content</h1>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast pizaa company.</Link>
+      <p>ALi</p>
+    </header>
+  );
+}
+
+export default Header;
