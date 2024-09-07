65a20cb30d8a0bad929f748cd9ef0a51e3a0af27
“Ali <tadayoni.ali.enayat@gmail.com>
2024-09-07 12:14:47 +0330

    add redux to project

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">ALI</div>;
+  const userName = useSelector((state) => state.user.userName);
+
+  if (!userName) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
 
 export default Username;
